69145c01f1ba353f2db6064c0fae1415b11a04f6
danadrianmirea <152083571+danadrianmirea@users.noreply.github.com>
2024-10-11 19:44:11 +0300

    add some more bloat apps to uninstall

--- delete.sh
+++ delete.sh
@@ -44,7 +44,44 @@ adb shell pm uninstall -k --user 0 com.android.providers.partnerbookmarks
 #bloat
 #adb shell pm uninstall -k --user 0 com.android.chrome
 adb shell pm uninstall -k --user 0 com.example.android.notepad
-adb shell pm uninstall -k --user 0 com.example.android.notepad
 adb shell pm uninstall -k --user 0 com.android.apps.tag
-
+adb shell pm uninstall -k --user 0 com.samsung.android.weather                                                    
+adb shell pm uninstall -k --user 0 com.android.backupconfirm                                                        
+adb shell pm uninstall -k --user 0 com.android.calllogbackup                                                        
+adb shell pm uninstall -k --user 0 com.android.sharedstoragebackup                                                  
+adb shell pm uninstall -k --user 0 com.android.wallpaperbackup                                                      
+adb shell pm uninstall -k --user 0 com.android.wallpapercropper                                                     
+adb shell pm uninstall -k --user 0 com.dsi.ant.sample.acquirechannels                                               
+adb shell pm uninstall -k --user 0 com.dsi.ant.server                                                               
+adb shell pm uninstall -k --user 0 com.enhance.gameservice                                                          
+adb shell pm uninstall -k --user 0 com.google.android.backuptransport                                               
+adb shell pm uninstall -k --user 0 com.google.android.partnersetup                                                  
+adb shell pm uninstall -k --user 0 com.samsung.android.app.galaxyfinder                                             
+adb shell pm uninstall -k --user 0 com.samsung.android.app.smartcapture                                             
+adb shell pm uninstall -k --user 0 com.samsung.android.email.provider                                               
+adb shell pm uninstall -k --user 0 com.samsung.android.fmm                                                          
+adb shell pm uninstall -k --user 0 com.samsung.android.keyguardwallpaperupdator                                     
+adb shell pm uninstall -k --user 0 com.samsung.android.knox.analytics.uploader                                      
+adb shell pm uninstall -k --user 0 com.samsung.android.knox.containeragent                                          
+adb shell pm uninstall -k --user 0 com.samsung.android.knox.containercore                                           
+adb shell pm uninstall -k --user 0 com.samsung.android.mobileservice                                                
+adb shell pm uninstall -k --user 0 com.samsung.android.rubin.app                                                    
+adb shell pm uninstall -k --user 0 com.samsung.android.shortcutbackupservice                                        
+adb shell pm uninstall -k --user 0 com.samsung.android.smartcallprovider                                            
+adb shell pm uninstall -k --user 0 com.samsung.android.smartface                                                    
+adb shell pm uninstall -k --user 0 com.samsung.android.smartmirroring                                               
+adb shell pm uninstall -k --user 0 com.samsung.klmsagent                                                            
+adb shell pm uninstall -k --user 0 com.samsung.knox.securefolder                                                    
+adb shell pm uninstall -k --user 0 com.samsung.knox.securefolder.setuppage                                          
+adb shell pm uninstall -k --user 0 com.sec.android.app.myfiles                                                      
+adb shell pm uninstall -k --user 0 com.sec.android.app.samsungapps                                                  
+adb shell pm uninstall -k --user 0 com.sec.android.app.wallpaperchooser                                             
+adb shell pm uninstall -k --user 0 com.sec.android.wallpapercropper2                                                
+adb shell pm uninstall -k --user 0 com.sec.android.widgetapp.samsungapps                                            
+adb shell pm uninstall -k --user 0 com.sec.enterprise.knox.attestation                                              
+adb shell pm uninstall -k --user 0 com.sec.enterprise.knox.cloudmdm.smdms                                           
+adb shell pm uninstall -k --user 0 com.sec.spp.push                                                                 
+adb shell pm uninstall -k --user 0 com.wssnps                                                                       
+adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall -k --user 0 com.knox.vpn.proxyhandler
 ##########
